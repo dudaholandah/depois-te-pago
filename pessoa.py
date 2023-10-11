73bca0f402f736b4be23bc4608ec577bbe99d8d3
Maria Eduarda Holanda <duda.mholanda@gmail.com>
2023-10-11 00:31:07 -0300

    - add class Pessoa

---
+++ pessoa.py
@@ -0,0 +1,6 @@
+class Pessoa:
+  def __init__(self, nome, saldo_pagou, saldo_envolvido, saldo_recebido):
+    self.nome = nome
+    self.saldo_pagou = saldo_pagou
+    self.saldo_envolvido = saldo_envolvido
+    self.saldo_recebido = saldo_recebido
