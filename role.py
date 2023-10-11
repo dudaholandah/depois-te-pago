285c3d92a511f5e6ebb89ec6c3447ccc28c25f42
Maria Eduarda Holanda <duda.mholanda@gmail.com>
2023-10-11 00:30:57 -0300

    - add class Role

---
+++ role.py
@@ -0,0 +1,20 @@
+from pessoa import Pessoa
+
+class Role:
+  def __init__(self, idx, nome, pagou : Pessoa, valor, envolvidos : list[Pessoa], transferiu : list[Pessoa], cada):
+    self.idx = idx
+    self.nome = nome
+    self.pagou = pagou
+    self.valor = valor
+    self.envolvidos = envolvidos
+    self.transferiu = transferiu
+    self.cada = cada #valor / len(envolvidos)
+    self.atualizar_pessoas()
+
+  def atualizar_pessoas(self):
+    self.pagou.saldo_pagou += self.valor
+    self.pagou.saldo_envolvido += self.cada 
+    
+    for each in self.envolvidos:
+      if each != self.pagou:
+        each.saldo_envolvido += self.cada
